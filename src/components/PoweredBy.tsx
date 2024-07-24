d06bcd7827bea2f7291fcb04acf4e0aa0b6efba9
prempro42 <prempro42@gmail.com>
2024-07-24 08:53:28 +0530

    added github repo link

--- src/components/PoweredBy.tsx
+++ src/components/PoweredBy.tsx
@@ -1,33 +1,15 @@
-import newTabIcon from "./../assets/newTab.ico";
-import reactLogo from "./../assets/react.svg";
-import tailwindIcon from "./../assets/tailwindcss.svg";
-import viteLogo from "/vite.svg";
+import githubIcon from "./../assets/icons8-github.svg";
 
 function PoweredBy() {
   return (
     <div className="absolute flex-col bottom-0 mb-5 w-1/2 md:w-1/4">
-      <p className="mb-2 justify-center flex dark:text-slate-300">Powered by</p>
       <div className=" flex ">
         <a
-          href="https://quotesnewtab.com/api/docs"
+          href="https://github.com/prempro42/random-quotes-app"
           target="_blank"
           className="w-9 m-auto "
         >
-          <img src={newTabIcon} alt="quotes new tab logo" />
-        </a>
-
-        <a href="https://react.dev" target="_blank" className="w-9 m-auto ">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a
-          href="https://tailwindcss.com"
-          target="_blank"
-          className="w-9 m-auto "
-        >
-          <img src={tailwindIcon} alt="Tailwind CSS logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" className="w-9 m-auto  ">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={githubIcon} alt="quotes new tab logo" />
         </a>
       </div>
     </div>
